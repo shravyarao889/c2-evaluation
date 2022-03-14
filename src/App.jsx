ec8286692b536c93a40f84eaad67ceb619844825
shravyarao889 <96167495+shravyarao889@users.noreply.github.com>
2022-03-14 11:02:16 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import { Rentals } from "./Components/Rentals/Rentals";
 import { AddHouse } from "./Components/AddHouse/AddHouse";
+import { useState } from "react";
 
 function App() {
+  const [show, setShow] = useState(true); 
   return (
     <div className="App">
-      <button className="toggleForm">
-        {/* Show text Add House or Show Rentals based on state */}
+      {show?<Rentals/>:<AddHouse/>}
+      <button className="toggleForm" onClick= {()=>{
+        setShow(show? false:true)
+      }}>{show?"Add House" : "Show Rentals"}
+        {         /* Show text Add House or Show Rentals based on state */}
+       
       </button>
       {/* Show component based on state */}
       <br />
--- src/Components/AddHouse/AddHouse.jsx
+++ src/Components/AddHouse/AddHouse.jsx
@@ -1,32 +1,78 @@
+import { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react/cjs/react.production.min";
+
 export const AddHouse = () => {
+  const [formData, setFormdata] = useState({
+    name: "",
+    ownerName: "",
+    address: "",
+    areaCode: "",
+    rent: "",
+    preferred_tenants: "",
+  });
+
+  const [getdatabaseData, setdatabaseData] = useState([]);
+
+  const handlechange = (e) => {
+    const preferred_tenants = e.target.id === "married" && e.target.checked === true ? "married" : "bachelor";
+    const { id, value } = e.target;
+
+    if (id === "married" || id === "bachelor") {
+      setFormdata({ ...formData, preferred_tenants: preferred_tenants });
+    }
+    else {
+      setFormdata({ ...formData, [id]: value });
+    }
+  }
+  useEffect(() => {
+    getData();
+  });
+
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+
+    axios.post("http://localhost:8080/houses", formData).then(() => {
+      alert("house added successfully");
+    });
+  }
+
+  const getData = () => {
+    axios.get("http://localhost:8080/houses").then((res) => {
+      setdatabaseData(res.data);
+  })
+}
+
+
   return (
     <div className="addHouseContainer">
-      <form>
+      <form onSubmit={handlesubmit}>
         <label>name</label>
-        <input type="text" className="name" value={""} required />
+        <input type="text" className="name"  required id="name" onChange={handlechange} />
         <br />
         <label>ownerName</label>
-        <input value={""} type="text" className="ownerName" required />
+        <input  type="text" className="ownerName" required id="ownerName" onChange={handlechange}/>
         <br />
         <label>address</label>
-        <input value={""} type="text" className="address" required />
+        <input type="text" className="address" required id="address" onChange={handlechange}/>
         <br />
         <label>areaCode</label>
-        <input value={""} type="text" className="areaCode" required />
+        <input  type="text" className="areaCode" required id="areaCode" onChange={handlechange} />
         <br />
         <label>rent</label>
-        <input value={""} type="text" className="rent" required />
+        <input  type="text" className="rent" required id="rent" onChange={handlechange} />
         <br />
         <label>preferredTenant</label>
         <br />
         <label>bachelor</label>
-        <input checked={""} type="checkbox" className="bachelor" />
+        <input  type="checkbox" className="bachelor" id="bachelor" onChange={handlechange} />
         <br />
         <label>married</label>
-        <input checked={""} type="checkbox" className="married" />
+        <input  type="checkbox" className="married" id="married" onChange={handlechange}/>
         <br />
         <label>image</label>
-        <input value={""} type="text" className="image" required />
+        <input  type="text" className="image"  id="image" onChange={handlechange} />
         <br />
         <input className="submitBtn" type="submit" />
       </form>
